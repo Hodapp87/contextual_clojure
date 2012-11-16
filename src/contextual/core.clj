25ccba5b340fa6ea305ce0b5ef23cc27b541cefc
Chris Hodapp <hodapp@hive13.org>
2012-11-15 22:29:24 -0500

    Progressed incrementally. Wrote compose-transform, put together a basic outline of rule-trampoline, modified 'rule' somewhat.

--- src/contextual/core.clj
+++ src/contextual/core.clj
@@ -1,10 +1,10 @@
-; If you use this namespace, then name this file for_the_glory_of_art.clj
-; and put it in the 'src' directory of the project (lein sets the classpath
-; here)   
+;; If you use this namespace, name this file for_the_glory_of_art.clj and put
+;; it in the 'src' directory of the project (lein sets the classpath here)   
 (ns contextual.core
   (:use quil.core))
-;(use 'clojure.repl)
+;;(use 'clojure.repl)
 
+;; This is Quil-specific.
 (defn setup []
   (smooth)
   (frame-rate 60)
@@ -15,43 +15,74 @@
   (let [x (mouse-x)  y (mouse-y)]
     (reset! (state :mouse-position) [x y])))
 
+;; This is not.
 
-
-; Return the default transform map, letting 'xform' override what is there;
-; e.g. (default-xform { :xscale 0.5 :yscale 0.5 })
+;; Return the default transform map, letting 'xform' override what is there;
+;; e.g. (default-xform { :xscale 0.5 :yscale 0.5 })
 (defn default-xform [{:as xform}]
-  (merge {:xscale 1 :yscale 1 :scale 1 :xtrans 0 :ytrans 0 :rotate 0
+  (merge {:xscale 1 :yscale 1 :xtrans 0 :ytrans 0 :rotate 0
           :xshear 0 :yshear 0 :hue 0 :sat 0 :brightness 0 :alpha 0
-          :child []}
+          :child nil}
          xform))
-; todo: fix scale/xscale/yscale inconsistency
+;; todo: fix scale/xscale/yscale inconsistency
 
-; Generate a rule tree given a list of transform properties, e.g.
-; e.g. (rule { :xscale 0.5 :child shape1 }
-;            { :yscale 0.5 :rotate 0.1 })
-; If the :child property is nil or is not given, then this is treated as the
-; rule instantiating itself recursively.
-(defn rule [& arglist]
-  (map default-xform arglist))
+;; Generate a rule tree given a list of transform properties, e.g.
+;; e.g. (rule rulename
+;;            { :xscale 0.5 :child shape1 }
+;;            { :yscale 0.5 :rotate 0.1 })
+;; 'ref' is an identifier - symbol, string, whatever you want. But if you want
+;; to refer to this rule from a :child field anywhere inside of this, you must
+;; use this same identifier there.
+;; Return value is like (ref xform xform xform...), where xform is a map with
+;; transform parameters.
+(defn rule [ref & xformlist]
+  (cons ref (map default-xform xformlist)))
 
-; Primitives (tentatively)
+;; Primitives (tentatively)
 (def square (symbol "square"))
 (def circle (symbol "circle"))
 ;; (def triangle (symbol "triangle"))
 ;; this will conflict as long as we have :use quil.core
 
-;; This is a special symbol that, when used as a child in a rule, signifies
-;; that the rule is to instantiate itself recursively.
-(def self (symbol "self"))
+;; Compose two transforms
+(defn compose-transform [xf1 xf2]
+  ;; All of the values on compose-fn are the functions which compose two
+  ;; parameters in a transform (i.e. :xscale is *, which means that if you
+  ;; compose two scale transforms, the amount multiplies)
+  ;; TODO: The color transforms may need clamping.
+  (let [compose-fn {:xscale * :yscale * :xtrans + :ytrans + :rotate + :xshear *
+                    :yshear * :hue + :sat + :brightness + :alpha *}]
+    ;; For every key in compose-fn, generate an (operation parameter) pair
+    ;; by composing the transform operations in xf1 and xf2 with the given
+    ;; functions; turn this list of (op param) into a map, like our inputs
+    (into {}
+          (map (fn [op] [op ((compose-fn op) (xf1 op) (xf2 op))])
+               (keys compose-fn)))))
 
-; If we lack the ability to explicitly get at our transformation matrix, then
-; we will have a hard time figuring out what the bounds of our canvas are.
-; However, we don't necessarily need this.
+(defn rule-trampoline [rule-tree tree-map]
+  ;; What we need to keep track of: Current transform.
+  ;; What we need to pass on: A tree; a map of any ref'ed symbols to resolve
+  ;; (yes, technically, if this is trampolining then we need to pass a function
+  ;; call utilizing these)
+  ;; What we need to do: Inspect our tree and figure out how to recurse further.
+  ;; Breadth-first or depth-first? (If and only if multiple rules are present)
+  ;; We must compose the transform with the next. We must also bail out at some
+  ;; point. Bailing out doesn't mean quitting the function - it could also mean
+  ;; moving onto another rule because the chain we've been on is drawing too
+  ;; small.
+  ;; We also don't handle any sort of randomness, hmmm...
+  (let [ref (first rule-tree)
+        xforms (rest rule-tree)
+        next-map (assoc tree-map ref rule-tree)]
+    ;; Here is where you left off.
+    nil)
 
-; shapefn is a function which draws the shape in question. For the sake of
-; sanity, it should always draw it centered at the origin; if this means
-; you need to translate it, then do it. The reason for this is that scaling
-; and rotation are always relative to the center.
+;; This is not yet organized:
+
+;; shapefn is a function which draws the shape in question. For the sake of
+;; sanity, it should always draw it centered at the origin; if this means
+;; you need to translate it, then do it. The reason for this is that scaling
+;; and rotation are always relative to the center.
 (defn recurse [shapefn rot tx ty sx sy]
   (defn rec [global_sx global_sy]
     (if (or (> global_sx 0.005) (> global_sy 0.005))
@@ -59,30 +90,30 @@
           (translate tx ty)
           (rotate rot)
           (scale sx sy)
-          ; sx2, sy2 keep track of global scale so that we know when to bail out.
+          ;; sx2, sy2 keep track of global scale so that we know when to bail out.
           (rec (* global_sx sx)
                (* global_sy sy)))))
-  ; We save our current matrix, and let 'rec' apply everything cumulatively.
+  ;; We save our current matrix, and let 'rec' apply everything cumulatively.
   (push-matrix)
   (rec 1.0 1.0)
-  ; Then return to where we started:
+  ;; Then return to where we started:
   (pop-matrix))
 
-; At a higher level, what 'recurse' does is:
-; (1) Receive a function.
-; (2) Receive a set of transforms that influences what happens upon calling
-; that function.
-; (3) Recursively call that function and apply those transforms.
-; (4) Bail out of that recursion when the transforms have hit some point, such
-; as the scale being too small for the shape to be visible. To do this, we
-; must know how to compose the transformations.
-; (5) Optionally: Keep track of the boundaries of things. We don't do this,
-; but it would be a nice thing to do, and Context Free does it.
-;
-; Even if we move into something with different semantics (say, stateless
-; drawing of shapes, not stateful like P5 and to some extent OpenGL), we can
-; probably still keep this all fundamentally the same just by generating some
-; closures that are coupled to each other.
+;; At a higher level, what 'recurse' does is:
+;; (1) Receive a function.
+;; (2) Receive a set of transforms that influences what happens upon calling
+;; that function.
+;; (3) Recursively call that function and apply those transforms.
+;; (4) Bail out of that recursion when the transforms have hit some point, such
+;; as the scale being too small for the shape to be visible. To do this, we
+;; must know how to compose the transformations.
+;; (5) Optionally: Keep track of the boundaries of things. We don't do this,
+;; but it would be a nice thing to do, and Context Free does it.
+;;
+;; Even if we move into something with different semantics (say, stateless
+;; drawing of shapes, not stateful like P5 and to some extent OpenGL), we can
+;; probably still keep this all fundamentally the same just by generating some
+;; closures that are coupled to each other.
 
 (defn square []
   (rect -0.5 -0.5 1 1))
@@ -93,16 +124,16 @@
 (defn start []
   (let [width 800
         height 800]
-    ; One demo I rather like, albeit simple. Move the mouse around a bit.
+    ;; One demo I rather like, albeit simple. Move the mouse around a bit.
     (defn draw_kaleid []
       (background 0)
-      ;(stroke 0 0 0 255)
+      ;;(stroke 0 0 0 255)
       (fill 0 255 0 32) 
-      ;(stroke-weight 2)
+      ;;(stroke-weight 2)
       (no-stroke)
       (let [[mouseX mouseY] @(state :mouse-position)
             side 1
-            ;shapefn #(rect (/ side -2) (/ side -2) side side)
+            ;;shapefn #(rect (/ side -2) (/ side -2) side side)
             shapefn square
             rot (- (* (/ mouseX width) 6.28) 3.14)
             sx (/ mouseY (* 1.05 height))
@@ -112,13 +143,13 @@
         (translate (/ side 2) (/ side 2))
         (scale width width)
         (recurse shapefn rot tx ty sx sy)))
-    ; r,g,b here are the fill color.
+    ;; r,g,b here are the fill color.
     (defn draw_kaleid2 []
       (background 0)
       (no-stroke)
       (let [[mouseX mouseY] @(state :mouse-position)
             side 1
-            ;shapefn #(rect (/ side -2) (/ side -2) side side)
+            ;;shapefn #(rect (/ side -2) (/ side -2) side side)
             shapefn square
             rot (- (* (/ mouseX width) 6.28) 3.14)
             sx (/ mouseY (* 1.05 height))
@@ -142,5 +173,3 @@
 
 (defn -main []
   (start))
-
-
--- src/contextual/experimental.clj
+++ src/contextual/experimental.clj
@@ -15,21 +15,21 @@
 ;; or I must specify a level (e.g. 0 for the same rule, 1 for the parent rule)
 ;; but this seems more hackish.
 
-; You don't need to answer whether or not it's a scene graph. You can just
-; generate an AST that happens to be a scene graph and also happens to be the
-; code that visualizes it, given an appropriate set of bindings.
+;; You don't need to answer whether or not it's a scene graph. You can just
+;; generate an AST that happens to be a scene graph and also happens to be the
+;; code that visualizes it, given an appropriate set of bindings.
 
-; Questions to still handle:
-; (1) How will I handle loops and tiling? How much of Lisp's parsing/evaluating
-; do I retain access to?
-; (2) Process of: (rule...) -> scene graph -> API calls/directives (P5, OpenGL,
-; Canvas, SVG, PDF...)
-; (3) What if the AST could include in parts of a scene graph?
-; (4) What is the null shape, for those cases where recursion should terminate
-; based on some probability value?
-; (5) How do we handle bailout from recursion?
-; (6) At some point, all my rules must reduce down to primitives. What are my
-; primitives? Particularly, in a Clojure sense, how do I make them?
-; (7) How do I represent randomness in the rules? i.e. like my rule-choose
-; structure
+;; Questions to still handle:
+;; (1) How will I handle loops and tiling? How much of Lisp's parsing/evaluating
+;; do I retain access to?
+;; (2) Process of: (rule...) -> scene graph -> API calls/directives (P5, OpenGL,
+;; Canvas, SVG, PDF...)
+;; (3) What if the AST could include in parts of a scene graph?
+;; (4) What is the null shape, for those cases where recursion should terminate
+;; based on some probability value?
+;; (5) How do we handle bailout from recursion?
+;; (6) At some point, all my rules must reduce down to primitives. What are my
+;; primitives? Particularly, in a Clojure sense, how do I make them?
+;; (7) How do I represent randomness in the rules? i.e. like my rule-choose
+;; structure
 
